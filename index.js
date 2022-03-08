db6d858ad9fb7480ff184caf87946e13c4866c69
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-08 15:20:37 -0800

    Server is listening on Port

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// require your server and launch it here
+
+const server = require('./api/server')
+
+
+const PORT = 8000
+
+server.listen(8000, () => {
+    console.log(`Server is Listeng on ${PORT}`)
+})
