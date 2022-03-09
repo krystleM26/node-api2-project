d70b6ca862da0ab2f7e782bc08d827715566236c
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-08 19:09:32 -0800

    Inserts new posts if body is given

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express')
+
+const postsRouter = require('./posts/posts-router')
 const server = express()
 server.use(express.json())
+
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req,res) => {
+    res.send('Posts Go Here')
+})
+
 
 module.exports = server;
